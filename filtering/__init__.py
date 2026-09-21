7e5bba1cf53e469bebc68dfcb27dbe1413f5b002
Igor Uchoa <31633303+igoruchoa@users.noreply.github.com>
2026-09-21 15:06:38 -0400

    Adding. a new batch of calude files

--- filtering/__init__.py
+++ filtering/__init__.py
@@ -3,7 +3,11 @@ from importlib.metadata import version, PackageNotFoundError
 try:
     __version__ = version("lagrangian-filtering")
 except PackageNotFoundError:
-    pass
+    # Fall back to the version file written by setuptools_scm at build time
+    try:
+        from filtering._version import version as __version__
+    except ImportError:
+        __version__ = "unknown"
 
 from filtering.filtering import LagrangeFilter
 import filtering.analysis
